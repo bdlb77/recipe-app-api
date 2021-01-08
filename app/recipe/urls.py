36599b5f06daf0c6f79fa59ae1fa30b07dcc300a
bryan leighton <bryan.leigh77@gmail.com>
2021-01-07 22:58:12 -0800

    -a

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ from recipe import views
 
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 # for reverse() to look up url
 app_name = 'recipe'
